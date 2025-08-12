7c2e832247daaa3114a4f0aa0f50ba7e219a2c41
Nisha Sethi <297.nishasethi@gmail.com>
2025-08-12 14:36:51 +0000

    Live Search Added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+], (Controller, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
@@ -14,6 +16,26 @@ sap.ui.define([
             this.oRouter.navTo('View2',{
                 invoiceId : sIndex
             });
+        },
+        onSearch: function(oEvent){
+            debugger;
+            var aFilters = [];
+            // var fFilter = new Filter();
+			var sQuery = oEvent.getSource().getValue();
+			if (sQuery && sQuery.length > 0) {
+				var filter1 = new Filter("vendorCode", FilterOperator.Contains, sQuery);
+                var filter2 = new Filter("companyCode", FilterOperator.Contains, sQuery);   
+                var oFinalFilter = new sap.ui.model.Filter({
+                filters: [filter1, filter2],
+                and: false
+            });
+				aFilters.push(oFinalFilter);
+			}
+
+			// update list binding
+			var oList = this.byId("idList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilters, "Application");
         }
     });
 });
--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -7,13 +7,25 @@ sap.ui.define(["sap/ui/core/mvc/Controller"
             this.oRouter.getRoute("View2").attachMatched(this._onObjectMatched, this);
         },
         _onObjectMatched: function(oEvent){
-            debugger;
             var sInvoiceId = oEvent.getParameter("arguments").invoiceId;
             var invoiceRelativePath = "/Invoices/" + sInvoiceId;
             this.getView().bindElement({
                 path: invoiceRelativePath,
                 model: "global" 
             });
+        },
+        onPost: function(){
+                var oHeader = this.getView().getBindingContext("global").getObject();
+
+                // Clone to avoid accidental model reference changes
+                var oHeaderCopy = JSON.parse(JSON.stringify(oHeader));
+
+                // Now oHeaderCopy contains:
+                // - updated header fields (invoiceNumber, vendorCode, etc.)
+                // - updated invoiceItems array with changed amounts, etc.
+
+                console.log("Header:", oHeaderCopy);
+                console.log("Items:", oHeaderCopy.invoiceItems);
         }
     });
 
